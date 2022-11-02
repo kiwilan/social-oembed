845ca58dfd64d06a77e9c93dfcc55fb4e07a0c36
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-11-02 19:32:07 +0100

    clean

--- .archive/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { dirname, join } from 'path'
-// Import plugins
-
-export default {
-  root: join(dirname(new URL(import.meta.url).pathname), 'client'),
-  plugins: [
-    // Register plugins
-  ]
-}
