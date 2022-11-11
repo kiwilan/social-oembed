083a7196e740616881f913bdce594ecefe04b674
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-11-11 12:09:57 +0100

    clean

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,11 +2,6 @@ import type { FastifyPluginAsync } from 'fastify'
 import { getRoute, route } from '~/utils/Route'
 
 const root: FastifyPluginAsync = async (fastify): Promise<void> => {
-  // fastify.get('/', async (req, res) => {
-  //   // res.redirect(301, getRoute('/api'))
-
-  // })
-
   fastify.route({
     method: 'GET',
     url: getRoute('/'),
