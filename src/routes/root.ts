3b96cbc31f540749a2c5ffdbc60a676ff7ebf3fe
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-11-11 12:09:07 +0100

    wip

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,25 @@
 import type { FastifyPluginAsync } from 'fastify'
-import { getRoute } from '~/utils/Route'
+import { getRoute, route } from '~/utils/Route'
 
 const root: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.get('/', async (req, res) => {
-    res.redirect(301, getRoute('/api'))
+  // fastify.get('/', async (req, res) => {
+  //   // res.redirect(301, getRoute('/api'))
+
+  // })
+
+  fastify.route({
+    method: 'GET',
+    url: getRoute('/'),
+    async handler() {
+      return {
+        meta: {
+          routes: {
+            api: route('/api'),
+            docs: route('/docs'),
+          }
+        }
+      }
+    },
   })
 }
 
